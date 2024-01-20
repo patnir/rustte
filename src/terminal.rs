bc5bd69167b06b7e04ac4da062d31a2599d7a1a5
Rahul Patni <rahul20patni@gmail.com>
2024-01-19 21:58:03 -0800

    welcome message

--- src/terminal.rs
+++ src/terminal.rs
@@ -2,11 +2,11 @@ use std::io::{self, stdout, Write};
 use termion::event::Key;
 use termion::input::TermRead;
 use termion::raw::{IntoRawMode, RawTerminal};
+
 pub struct Size {
     pub width: u16,
     pub height: u16,
 }
-
 pub struct Terminal {
     size: Size,
     _stdout: RawTerminal<std::io::Stdout>,
@@ -29,22 +29,28 @@ impl Terminal {
     pub fn clear_screen() {
         print!("{}", termion::clear::All);
     }
-
     pub fn cursor_position(x: u16, y: u16) {
         let x = x.saturating_add(1);
         let y = y.saturating_add(1);
         print!("{}", termion::cursor::Goto(x, y));
     }
-
     pub fn flush() -> Result<(), std::io::Error> {
         io::stdout().flush()
     }
-
     pub fn read_key() -> Result<Key, std::io::Error> {
         loop {
             if let Some(key) = io::stdin().lock().keys().next() {
                 return key;
             }
         }
+    }
+    pub fn cursor_hide() {
+        print!("{}", termion::cursor::Hide);
+    }
+    pub fn cursor_show() {
+        print!("{}", termion::cursor::Show);
+    }
+    pub fn clear_current_line() {
+        print!("{}", termion::clear::CurrentLine);
     }
 }
