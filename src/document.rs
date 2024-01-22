1ed6a26fd879f96784fa77854fcb217504ecffb4
Rahul Patni <rahul20patni@gmail.com>
2024-01-21 19:40:43 -0800

    render row method

--- src/document.rs
+++ src/document.rs
@@ -4,3 +4,18 @@ use crate::Row;
 pub struct Document {
     rows: Vec<Row>,
 }
+
+impl Document {
+    pub fn open() -> Self {
+        let mut rows = Vec::new();
+        rows.push(Row::from("Hello World!"));
+        Self { rows }
+    }
+
+    pub fn row(&self, index: usize) -> Option<&Row> {
+        self.rows.get(index)
+    }
+    pub fn is_empty(&self) -> bool {
+        self.rows.is_empty()
+    }
+}
